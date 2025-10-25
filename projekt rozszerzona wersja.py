6870d0ed3feaceca5b896d7db1d10e1e025b36df
Magdalena Dąbrowska <magdalenadabrowska122@gmail.com>
2025-10-25 23:02:30 +0200

    Wyszukiwanie 3 motywów w genie TP53

--- projekt rozszerzona wersja.py
+++ projekt rozszerzona wersja.py
@@ -7,3 +7,24 @@ genbank_id = "NM_000546"
 a = Entrez.efetch(db="nucleotide", id=genbank_id, rettype="fasta", retmode="text")
 b = SeqIO.read(a, "fasta")
 a.close()
+print("ID rekordu:", b.id)
+print("Opis:", b.description)
+print("Pierwsze 50 nukleotydów:", b.seq[:50])
+print("Długość całej sekwencji:", len(b.seq))
+# Wyszukanie wielu motywów jednoczenie. Wybrałam motywy CAG, CCC, ATG
+motywy = ["CAG", "ATG", "CCC"]
+# Postaram się użyć słownika
+wyniki = {}
+for motyw in motywy:
+    pozycje = []
+    start = 0
+    while True:
+        idx = b.seq.find(motyw, start)
+        if idx == -1:
+            break
+        pozycje.append(idx)
+        start = idx + 1
+    wyniki[motyw] = pozycje
+
+for motyw, pozycje in wyniki.items():
+    print(f"Motyw {motyw} występuje {len(pozycje)} razy na pozycjach: {pozycje[:10]}{'...' if len(pozycje) > 10 else ''}")
